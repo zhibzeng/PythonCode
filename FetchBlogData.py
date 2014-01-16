b203bac40b3042f918290db5943dd76d3e91c489
曾质彬 <zhibzeng@gmail.com>
2014-01-16 13:52:32 +0800

    fetch blog data change

--- FetchBlogData.py
+++ FetchBlogData.py
@@ -1,21 +1,36 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# Name:        Fetch blog data
+# Purpose:     Fetch files from a specific blog
 #
-# Author:      Administrator
+# Author:      Jeffrey.zeng
 #
 # Created:     15/01/2014
-# Copyright:   (c) Administrator 2014
+# Copyright:   (c) Jeffrey.zeng 2014
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 from urllib.request import urlopen
+from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
 import re
+import os
 url = "http://www.fmatlab.com/"
 pageHtml = urlopen(url).read().decode('utf-8')
 soup = BeautifulSoup(pageHtml)
 linksDiv = soup.find('div',class_='entry-content clearfix')
 links = linksDiv.find_all('p')
+path = 'C:\\FinanceMatlabBook\\'
+if not os.path.isdir(path):
+    os.makedirs(path)
 
 for link in links:
     print('source url-> '+link.a['href'])
+    linkurl = link.a['href']
+    title = link.a.get_text()
+    chapter = link.contents[0]
+    print(linkurl)
+    print(chapter)
+    print(title)
+    urlretrieve(linkurl,path+'('+chapter+')'+title+'.mht')
+
+
+
