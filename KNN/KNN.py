daa2f1e3eb9c2d5b930e18600176d533bc138e85
曾质彬 <zhibzeng@gmail.com>
2014-02-24 17:35:19 +0800

    modify KNN.py

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,19 +1,35 @@
 __author__ = 'JeffreyTseng'
 from numpy import *
 import operator
-import matplotlib as mpl
 
 
 def createDataSet():
-    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-    labels = ['A', 'B', 'C', 'D']
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1], [0.01, 0.01], [1.0, 1.11]])
+    labels = ['A', 'B', 'C', 'D', 'C', 'A']
     return group, labels
 
 
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
 def main():
-    group, label = createDataSet()
+    group, labels = createDataSet()
     print(group)
-    print(label)
+    print(labels)
+    result = classify0([0, 0], group, labels, 3)
+    print('result='+result)
 
 
 if __name__ == "__main__":
