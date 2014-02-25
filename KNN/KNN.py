0080e8bf68c5d78c25bf6d3d6065626a2c40c4b2
曾质彬 <zhibzeng@gmail.com>
2014-02-25 17:10:32 +0800

    modify files

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -23,13 +23,52 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+def File2Matrix(filename):
+    fileOpen = open(filename)
+    fileOnLines = fileOpen.readlines()
+    lines = len(fileOnLines)
+    returnMat = zeros((lines, 3)) #Create a lines X 3 matrix
+    labelsVector = []
+    index = 0
+    for line in fileOnLines:
+        line = line.strip()
+        dataList = line.split('\t')
+        returnMat[index, :] = dataList[0:3]
+        labelsVector.append(int(dataList[-1]))
+        index += 1
+    return returnMat, labelsVector
+
+
+def autoNum(dataset):
+    minValue = dataset.min()
+    maxValue = dataset.max()
+    ranges = maxValue - minValue
+    normDataset = zeros(shape(dataset))
+    rows = dataset.shape[0]
+    normDataset = dataset - tile(minValue, (rows, 1))
+    normDataset = normDataset / tile(ranges, (rows, 1))
+    return normDataset, ranges, minValue
+
+
+
 
 def main():
-    group, labels = createDataSet()
-    print(group)
-    print(labels)
-    result = classify0([0, 0], group, labels, 3)
-    print('result='+result)
+    # group, labels = createDataSet()
+    # print(group)
+    # print(labels)
+    # result = classify0([0, 0], group, labels, 3)
+    # print('result='+result)
+    returnMat, labelVector = File2Matrix('datingTestSet2.txt')
+    norMat, ranges, minValues = autoNum(returnMat)
+    print(norMat[0, :])
+    import matplotlib
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(returnMat[:, 1], returnMat[:, 2], 15.0*array(labelVector), 15.0*array(labelVector))
+    plt.show()
+
+
 
 
 if __name__ == "__main__":
